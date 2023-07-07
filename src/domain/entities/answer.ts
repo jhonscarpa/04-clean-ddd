69996ecbc9fcec3d5a6d9b51dd183464e42c17c3
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-07-07 18:26:44 -0300

    Fundamentos de DDD - Classe base de entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from 'node:crypto'
+import { Entity } from '../../core/entities/entity'
 
 interface IPropsAnswer {
   content: string
@@ -6,16 +6,10 @@ interface IPropsAnswer {
   questionId: string
 }
 
-export class Answer {
-  public id: string
-  public content: string
-  public authorId: string
-  public questionId: string
-
-  constructor(props: IPropsAnswer, id?: string) {
-    this.content = props.content
-    this.authorId = props.authorId
-    this.questionId = props.questionId
-    this.id = id ?? randomUUID()
+export class Answer extends Entity<IPropsAnswer> {
+  get content() {
+    return this.props.content
   }
+
+
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,5 +1,5 @@
-import { randomUUID } from 'node:crypto'
 import { Slug } from './value-objects/slug'
+import { Entity } from '../../core/entities/entity'
 
 interface IPropsQuestion {
   title: string
@@ -8,18 +8,6 @@ interface IPropsQuestion {
   slug: Slug
 }
 
-export class Question {
-  public id: string
-  public title: string
-  public slug: Slug
-  public content: string
-  public authorId: string
-
-  constructor(props: IPropsQuestion, id?: string) {
-    this.title = props.title
-    this.content = props.content
-    this.slug = props.slug
-    this.authorId = props.authorId
-    this.id = id ?? randomUUID()
-  }
+export class Question extends Entity<IPropsQuestion> {
+ 
 }
