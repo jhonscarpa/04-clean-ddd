4aca3ab51734274890f9eb9b628ea403aca3ab72
Jhonatan Felipe <83676609+jhonscarpa@users.noreply.github.com>
2023-07-15 21:02:05 -0300

    create default errors

--- src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -2,14 +2,14 @@ import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-an
 import { DeleteAnswerCommentUseCase } from './delete-answer-comment'
 import { makeAnswerComment } from 'test/factories/make-answer-comment'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
 let sut: DeleteAnswerCommentUseCase
 
 describe('Delete Answer Comment', () => {
   beforeEach(() => {
-    inMemoryAnswerCommentsRepository =
-      new InMemoryAnswerCommentsRepository()
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
     sut = new DeleteAnswerCommentUseCase(inMemoryAnswerCommentsRepository)
   })
   it('should be able to delete a answer comment', async () => {
@@ -30,12 +30,11 @@ describe('Delete Answer Comment', () => {
     })
 
     await inMemoryAnswerCommentsRepository.create(answerComment)
-
-    await expect(() =>
-      sut.execute({
-        answerCommentId: answerComment.id.toString(),
-        authorId: 'author-2',
-      }),
-    ).rejects.toBeInstanceOf(Error)
+    const result = await sut.execute({
+      answerCommentId: answerComment.id.toString(),
+      authorId: 'author-2',
+    })
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
